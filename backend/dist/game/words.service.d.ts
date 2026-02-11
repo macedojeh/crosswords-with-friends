327d483a226688c77202b8e7dd3a610d93d065a8
jessicaamacedo <jessica.macedo@somagrupo.com.br>
2026-02-10 22:50:15 -0300

    remove arquivos desnecessarios

--- backend/dist/game/words.service.d.ts
+++
@@ -1,9 +0,0 @@
-export declare class WordsService {
-    private readonly words;
-    private readonly clues;
-    constructor();
-    getAllWords(): string[];
-    getClue(word: string): string;
-    getRandomWords(count: number): string[];
-    getTotalCount(): number;
-}
